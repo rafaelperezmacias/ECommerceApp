b11cab33b92ee6571eca75cbaec1a2d7d10541d4
Rafael Pérez <rafaelpemaceti25@gmail.com>
2024-06-21 18:38:54 -0600

    - Correccion de las librerias de hilt/dagger. - Se agrego la prevializacion de los contenidos en le main activity (xml).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
     id("kotlin-parcelize")
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.gms.google-services")
-    id("kotlin-kapt")
 }
 
 android {
@@ -61,12 +61,12 @@ dependencies {
 
     //Dagger - Hilt
     implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation(libs.androidx.hilt.lifecycle.viewmodel)
     kapt(libs.androidx.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
-    // Kotlin Extensions and Coroutines support for room
+    // Kotlin Extensions and Coroutines support for Room
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.room.runtime)
     //noinspection KaptUsageInsteadOfKsp
